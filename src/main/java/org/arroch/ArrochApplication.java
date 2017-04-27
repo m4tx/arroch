692777701af6974e18ba9466e2bd39e2282b6e81
Mateusz Maćkowski <m4tx@m4tx.pl>
2017-04-27 17:41:30 +0200

    Initial commit with Spring framework hello world

---
+++ src/main/java/org/arroch/ArrochApplication.java
@@ -0,0 +1,11 @@
+package org.arroch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrochApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ArrochApplication.class, args);
+    }
+}
