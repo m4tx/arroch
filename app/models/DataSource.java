dea28e6e48f4e449511980428619225469bcfb94
Adam Pardyl <adam@pardyl.com>
2017-05-05 23:02:23 +0200

    Add DataSource Model

---
+++ app/models/DataSource.java
@@ -0,0 +1,26 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "sources")
+public class DataSource {
+    @Id @GeneratedValue
+    @Column(name = "source_id")
+    private int id;
+
+    @Column
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
