cf8b724639af78432cd2c9bb9d043860a1a395c6
Mateusz Maćkowski <m4tx@m4tx.pl>
2017-05-26 11:48:17 +0200

    Fix tests after upgrading to Play 2.6

--- test/controllers/people/EditPersonWebTests.java
+++ test/controllers/people/EditPersonWebTests.java
@@ -14,6 +14,11 @@ public class EditPersonWebTests extends WithBrowserAndTestDatabase {
     private static final String TEST_LAST_NAME = "Schrute";
     private static final String TEST_DISPLAY_NAME = "Dwight K. Schrute III";
 
+    public static final String FIRST_NAME_SELECTOR = "#firstName";
+    public static final String MIDDLE_NAME_SELECTOR = "#middleName";
+    public static final String LAST_NAME_SELECTOR = "#lastName";
+    public static final String DISPLAY_NAME_SELECTOR = "#displayName";
+
     private Person createTestPerson(JPAApi jpaApi) {
         final Person[] person = new Person[1];
         jpaApi.withTransaction(() -> {
@@ -32,9 +37,29 @@ public class EditPersonWebTests extends WithBrowserAndTestDatabase {
     public void testFormIsFilledOut() {
         JPAApi jpaApi = app.injector().instanceOf(JPAApi.class);
         browser.goTo(controllers.people.routes.EditPerson.get(createTestPerson(jpaApi).getId()).url());
-        assertThat(browser.$("#firstName").getAttribute("value")).isEqualTo(TEST_FIRST_NAME);
-        assertThat(browser.$("#middleName").getAttribute("value")).isEqualTo(TEST_MIDDLE_NAME);
-        assertThat(browser.$("#lastName").getAttribute("value")).isEqualTo(TEST_LAST_NAME);
-        assertThat(browser.$("#displayName").getAttribute("value")).isEqualTo(TEST_DISPLAY_NAME);
+        assertThat(browser.$(FIRST_NAME_SELECTOR).value()).isEqualTo(TEST_FIRST_NAME);
+        assertThat(browser.$(MIDDLE_NAME_SELECTOR).value()).isEqualTo(TEST_MIDDLE_NAME);
+        assertThat(browser.$(LAST_NAME_SELECTOR).value()).isEqualTo(TEST_LAST_NAME);
+        assertThat(browser.$(DISPLAY_NAME_SELECTOR).value()).isEqualTo(TEST_DISPLAY_NAME);
+    }
+
+    @Test
+    public void testEditPerson() {
+        JPAApi jpaApi = app.injector().instanceOf(JPAApi.class);
+        Person person = createTestPerson(jpaApi);
+        browser.goTo(controllers.people.routes.EditPerson.get(person.getId()).url());
+        browser.$(FIRST_NAME_SELECTOR).first().fill().with("A");
+        browser.$(MIDDLE_NAME_SELECTOR).first().fill().with("B");
+        browser.$(LAST_NAME_SELECTOR).first().fill().with("C");
+        browser.$(DISPLAY_NAME_SELECTOR).first().fill().with("D");
+        browser.$("button[type=submit]").click();
+        waitForPageLoad();
+        jpaApi.withTransaction(() -> {
+            Person refreshedPerson = jpaApi.em().find(Person.class, person.getId());
+            assertThat(refreshedPerson.getFirstName()).isEqualTo("A");
+            assertThat(refreshedPerson.getMiddleName()).isEqualTo("B");
+            assertThat(refreshedPerson.getLastName()).isEqualTo("C");
+            assertThat(refreshedPerson.getDisplayName()).isEqualTo("D");
+        });
     }
 }
--- test/utils/WithBrowserAndTestDatabase.java
+++ test/utils/WithBrowserAndTestDatabase.java
@@ -4,10 +4,16 @@ import play.Application;
 import play.inject.guice.GuiceApplicationBuilder;
 import play.test.WithBrowser;
 
+import java.util.concurrent.TimeUnit;
+
 import static utils.WithTestDatabase.configureTestDatabase;
 
 
 public class WithBrowserAndTestDatabase extends WithBrowser {
+    protected void waitForPageLoad() {
+        browser.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+    }
+
     @Override
     protected final Application provideApplication() {
         return configureTestDatabase(new GuiceApplicationBuilder()).build();
