5184e24048de3f2595a17a01e0c77e7b968a60b4
Adam Pardyl <adam@pardyl.com>
2017-05-27 21:09:32 +0200

    Extend DataSource model

--- app/models/DataSource.java
+++ app/models/DataSource.java
@@ -13,8 +13,14 @@ public class DataSource {
     @Column(name = "source_id")
     private long id;
 
-    @Column(length = 50)
+    @Column(length = 30)
     private String name;
+
+    @Column(length = 15)
+    private String icon;
+
+    @Column(length = 50)
+    private String url;
 
     public long getId() {
         return id;
@@ -28,10 +34,37 @@ public class DataSource {
         this.name = name;
     }
 
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
     static {
         DatabasePreloader.addDefault(em -> {
             DataSource d = new DataSource();
             d.setName("Arroch");
+            d.setIcon("database");
+            em.persist(d);
+            d = new DataSource();
+            d.setName("Google");
+            d.setIcon("google");
+            d.setUrl("https://plus.google.com/");
+            em.persist(d);
+            d = new DataSource();
+            d.setName("Facebook");
+            d.setIcon("facebook");
+            d.setUrl("https://www.facebook.com/");
             em.persist(d);
         }, 0);
     }
