26957eebec4a0117b36480cd585e45aefd131635
Adam Pardyl <adam@pardyl.com>
2017-05-29 00:44:41 +0200

    Fix DatabasePreloader error when fetching existing database

--- app/modules/preloader/DatabasePreloader.java
+++ app/modules/preloader/DatabasePreloader.java
@@ -2,6 +2,7 @@ package modules.preloader;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import models.DataSource;
 import play.Logger;
 import play.api.inject.ApplicationLifecycle;
 import play.db.jpa.JPAApi;
@@ -9,6 +10,12 @@ import play.db.jpa.JPAApi;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.PersistenceUnitUtil;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.io.ObjectInputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.AbstractMap;
@@ -44,6 +51,14 @@ public class DatabasePreloader {
             jpa.withTransaction(() -> {
                 EntityManager em = jpa.em();
 
+                CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+                CriteriaQuery<Long> query = criteriaBuilder.createQuery(Long.class);
+                query.select(criteriaBuilder.count(query.from(DataSource.class)));
+                if (em.createQuery(query).getSingleResult() != 0) {
+                    Logger.info("Database not empty, skipping preload");
+                    testEnabled = false;
+                    return;
+                }
 
                 defaultTasks.sort(Comparator.comparingInt(Entry::getKey));
                 for (Entry<Integer, Class<?>> a : defaultTasks) {
@@ -74,11 +89,17 @@ public class DatabasePreloader {
 
     private void loadDefault(Class<?> valueMap, EntityManager em) {
         Field[] declaredFields = valueMap.getDeclaredFields();
+        PersistenceUnitUtil emUtils = em.getEntityManagerFactory().getPersistenceUnitUtil();
         for (Field field : declaredFields) {
             if (Modifier.isStatic(field.getModifiers()) && field.isAnnotationPresent(DefaultValue.class)) {
                 try {
-                    Object c = field.get(null);
-                    em.persist(c);
+                    Object val = field.get(null);
+                    Object oldVal = em.find(val.getClass(), emUtils.getIdentifier(val));
+                    if(oldVal == null) {
+                        em.persist(val);
+                    } else {
+                        field.set(null, oldVal);
+                    }
                 } catch (IllegalAccessException e) {
                     throw new RuntimeException("Cannot Access default model value field\n" + e.toString());
                 }
