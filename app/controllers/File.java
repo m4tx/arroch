cb99402f9bb77b9315e464a5f034d5e06e258c19
Adam Pardyl <adam@pardyl.com>
2017-05-29 01:16:55 +0200

    Add File preview + image minifying

--- app/controllers/File.java
+++ app/controllers/File.java
@@ -4,6 +4,8 @@ import play.db.jpa.JPA;
 import play.db.jpa.Transactional;
 import play.mvc.Result;
 
+import java.io.IOException;
+
 import static play.mvc.Results.ok;
 
 public class File {
@@ -11,6 +13,13 @@ public class File {
     public Result get(Long id) {
         models.FileMeta fileMeta = JPA.em().find(models.FileMeta.class, id);
         java.io.File file = models.FileManager.getFile(fileMeta);
+        return ok(file).as(fileMeta.getMimeType());
+    }
+
+    @Transactional
+    public Result getPreview(Long id) throws IOException {
+        models.FileMeta fileMeta = JPA.em().find(models.FileMeta.class, id);
+        java.io.File file = models.FileManager.getThumbnail(fileMeta);
         return ok(file).as(fileMeta.getMimeType());
     }
 }
--- app/models/FileManager.java
+++ app/models/FileManager.java
@@ -5,10 +5,16 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import play.Logger;
 
+import javax.imageio.ImageIO;
 import javax.persistence.EntityManager;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+
+import static java.awt.RenderingHints.*;
+import static java.awt.RenderingHints.VALUE_INTERPOLATION_BILINEAR;
 
 public class FileManager {
     private static String filePath = ConfigFactory.load().getString("filePath");
@@ -42,12 +48,53 @@ public class FileManager {
         return f;
     }
 
+    public static void createDirectories() {
+        new File(filePath + "/min").mkdirs();
+    }
+
     public static void deleteAllFiles() throws IOException {
         Logger.warn("!!!Purging files directory!!!");
         FileUtils.cleanDirectory(new File(filePath));
+        createDirectories();
     }
 
     public static File getFile(FileMeta meta) {
         return new File(getFilePath(meta));
+    }
+
+    private static BufferedImage minifyImg(BufferedImage orig, int height, int width) {
+        double outputAspect = 1.0*width/height;
+        double inputAspect = 1.0*orig.getWidth()/orig.getHeight();
+        if (outputAspect < inputAspect) {
+            height = (int)(width/inputAspect);
+        } else {
+            width = (int)(height*inputAspect);
+        }
+        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = (Graphics2D) img.getGraphics();
+        g.setRenderingHint(KEY_INTERPOLATION, VALUE_INTERPOLATION_BILINEAR);
+        g.drawImage(orig, 0, 0, width, height, null);
+        g.dispose();
+        return img;
+    }
+
+    public static String getThumbnailPath(FileMeta meta) {
+        return filePath + "/min/" + meta.getId() + '.' + meta.getExtension();
+    }
+
+    public static File getThumbnail(FileMeta meta) throws IOException {
+        if (!meta.getMimeType().equals("image/jpeg") && !meta.getMimeType().equals("image/png")) {
+            return new File("public/images/file.svg");
+        }
+
+        File img = new File(getFilePath(meta));
+        File thumbnail = new File(getThumbnailPath(meta));
+        if (!thumbnail.exists() || (thumbnail.lastModified() < img.lastModified())) {
+            BufferedImage orig = ImageIO.read(img);
+            BufferedImage thumb = minifyImg(orig, 100, 100);
+            ImageIO.write(thumb, "jpg", thumbnail);
+        }
+
+        return thumbnail;
     }
 }
