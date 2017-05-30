b1d4b08b73d5540556967d34ae9d3f956b56bdca
Adam Pardyl <adam@pardyl.com>
2017-05-30 20:56:16 +0200

    Add person groups, conversations and activity pages (without content)

--- app/controllers/people/Person.java
+++ app/controllers/people/Person.java
@@ -3,12 +3,10 @@ package controllers.people;
 import play.db.jpa.JPA;
 import play.db.jpa.Transactional;
 import play.mvc.Result;
-import views.html.personInfo;
-import views.html.personAccounts;
-import views.html.personFriends;
-import views.html.personFriendOf;
+import utils.SimpleQuery;
+import views.html.*;
 
-import javax.persistence.EntityManager;
+import java.util.List;
 
 import static play.mvc.Results.ok;
 
@@ -46,5 +44,24 @@ public class Person {
     @Transactional
     public Result getFriendOfPage(Long id, int page) {
         return ok(personFriendOf.render(JPA.em().find(models.Person.class, id), 25, page));
+    }
+
+    @Transactional
+    public Result getActivity(Long id) {
+        return ok(personActivity.render(JPA.em().find(models.Person.class, id)));
+    }
+
+    @Transactional
+    public Result getConversations(Long id) {
+        return ok(personConversations.render(JPA.em().find(models.Person.class, id)));
+    }
+
+    @Transactional
+    public Result getGroups(Long id) {
+        models.Person person = JPA.em().find(models.Person.class, id);
+        List<models.Group> groups = (new SimpleQuery<>(JPA.em(), models.Group.class))
+                .where("type", models.GroupType.GroupTypeList.social)
+                .getResultList();
+        return ok(personGroups.render(person, groups));
     }
 }
