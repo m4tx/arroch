7774fae50afcfb19a4e9c1954b69e64f902c9f05
Mateusz Maćkowski <m4tx@m4tx.pl>
2017-06-10 11:31:09 +0200

    Add File info page

--- app/controllers/File.java
+++ app/controllers/File.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import models.FileManager;
+import models.FileMeta;
 import play.db.jpa.JPA;
 import play.db.jpa.Transactional;
 import play.mvc.Result;
+import views.html.pages.group.fileInfo;
 
 import java.io.IOException;
 
@@ -21,5 +24,12 @@ public class File {
         models.FileMeta fileMeta = JPA.em().find(models.FileMeta.class, id);
         java.io.File file = models.FileManager.getThumbnail(fileMeta);
         return ok(file);
+    }
+
+    @Transactional
+    public Result getInfo(Long id) {
+        FileMeta meta = JPA.em().find(models.FileMeta.class, id);
+        long length = FileManager.getFile(meta).length();
+        return ok(fileInfo.render(meta, length));
     }
 }
